fe4ab1e32cd1185b3d76ea633040b9b8332cf335
zzxinming <2021201560@ruc.edu.cn>
2023-11-16 23:25:26 +0800

    Add index page

--- watchlist/app.py
+++ watchlist/app.py
@@ -3,21 +3,27 @@ app = Flask(__name__)
 
 from flask import url_for
 from markupsafe import escape
-@app.route('/')
-def hello():
-    return 'Hello'
+from flask import Flask, render_template
+
 
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
+name = 'ZXM'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
