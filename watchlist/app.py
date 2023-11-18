6950bf6ab2ec52ae73a833322fcdc347cd2d7512
zzxinming <2021201560@ruc.edu.cn>
2023-11-18 22:33:45 +0800

    Add base template and error template

--- watchlist/app.py
+++ watchlist/app.py
@@ -1,9 +1,9 @@
-
+import click
 import os
 import sys
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -21,13 +21,12 @@ class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
 
-
 class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
-import click
+
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
 def initdb(drop):
@@ -37,15 +36,13 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
 
-
-import click
 @app.cli.command()
 def forge():
     """Generate fake data."""
     db.create_all()
 
     # 全局的两个变量移动到这个函数内
-    name = 'zzxinming'
+    name = 'Grey Li'
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -67,3 +64,19 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+
